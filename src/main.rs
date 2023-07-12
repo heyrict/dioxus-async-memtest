38c08b5b4f39205c04b1052ad35d8858e72eafda
heyrict <xiezh0831@yahoo.co.jp>
2023-07-12 10:18:18 +0800

    Add reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 use dioxus::prelude::*;
-use tokio_stream::StreamExt;
 
-#[inline_props]
-fn number_display(cx: Scope, number: u32) -> Element {
-    cx.render(rsx! {
-        span {
-            "{number}"
-        }
-    })
+struct Interface(pub reqwest::Client);
+
+impl Interface {
+    pub fn new() -> Self {
+        let client = reqwest::Client::builder()
+            .timeout(std::time::Duration::from_secs(60))
+            .cookie_store(true)
+            .build()
+            .unwrap();
+        Self(client)
+    }
 }
 
 fn app(cx: Scope) -> Element {
-    let number = use_state::<u32>(&cx, || 0);
+    use_shared_state_provider::<Interface>(&cx, || Interface::new());
+    let intf = use_shared_state::<Interface>(&cx).unwrap();
     let loading = use_state::<bool>(&cx, || false);
+    let text = use_state::<String>(&cx, || String::new());
 
     let handle_click = move |_| {
         use tokio::time::{sleep, Duration};
 
-        to_owned![number, loading];
+        to_owned![intf, text, loading];
         cx.spawn(async move {
-            let mut inner_stream = tokio_stream::iter(1..1000);
-            while let Some(i) = inner_stream.next().await {
+            let intf = &intf.read().0;
+            for i in 1..100 {
                 loading.set(true);
-                sleep(Duration::new(0, 50_000)).await;
-                number.set(i);
+                sleep(Duration::new(0, 100_000)).await;
+                let response_expect =
+                    format!("Failed to get text http://127.0.0.1:8080/{}.txt", &i);
+                let response = intf
+                    .get(format!("http://127.0.0.1:8080/{}.txt", &i))
+                    .send()
+                    .await
+                    .expect(&response_expect);
+                let content = response.text().await.expect("Failed to parse text");
+                text.set(content);
                 loading.set(false);
-                sleep(Duration::new(0, 200_000)).await;
             }
         })
     };
@@ -42,9 +54,7 @@ fn app(cx: Scope) -> Element {
                 onclick: handle_click,
                 "Start fetch"
             }
-            number_display {
-                number: *number.get(),
-            }
+            "{text}"
             loading_block
         }
     })
